426a668cce5681408c5c7f8b272f7be28477dcb2
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-17 07:41:29 -0300

    GetAccount test

---
+++ demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using kleversdk.provider;
+using kleversdk.provider.Dto;
+
+namespace demo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var kp = new KleverProvider(new NetworkConfig(Network.MainNet));
+            Task<AccountDto> t = kp.GetAccount("klv1j6z5yzhaxdp0am0ewnv0tczr9nhnwdt08maz9p6xegcq3uyhcxtq6eu9qz");
+            t.Wait();
+            var acc = t.Result;
+            
+            Console.WriteLine(acc.String());
+        }
+    }
+}
