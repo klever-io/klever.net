844d91218552f6ee59b8f522dce35489ab891653
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-17 07:39:45 -0300

    Init provider

---
+++ kleversdk/provider/IKleverProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using kleversdk.provider.Dto;
+
+namespace kleversdk.provider
+{
+    public interface IKleverProvider
+    {
+        /// <summary>
+        /// This endpoint allows one to retrieve basic information about an Address (Account).
+        /// </summary>
+        /// <param name="address">The address</param>
+        /// <returns><see cref="AccountDataDto"/></returns>
+        Task<AccountDto> GetAccount(string address);
+    }
+}
