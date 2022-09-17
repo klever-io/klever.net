4029937055221865d9250e6769fa25aff897c025
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-17 07:40:15 -0300

    API exception

---
+++ kleversdk/provider/Exceptions/APIException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace kleversdk.provider.Exceptions
+{
+    public class APIException : Exception
+    {
+        public APIException(string errorMessage, string code)
+        : base($"Error when calling API : {errorMessage}: {code}")
+        {
+        }
+
+    }
+}
