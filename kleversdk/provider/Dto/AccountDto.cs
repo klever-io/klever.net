3353d071e410ba1f1805196eebec6e31d28c9f1f
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-17 07:40:02 -0300

    Account and assets DTO

---
+++ kleversdk/provider/Dto/AccountDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using kleversdk.provider.Helper;
+
+namespace kleversdk.provider.Dto
+{
+    public class AccountDataDto
+    {
+        public AccountDto Account { get; set; }
+    }
+
+    public class AccountDto
+    {
+        public string Address { get; set; }
+        public long Nonce { get; set; }
+        public string Username { get; set; }
+        public string RootHash { get; set; }
+        public string Balance { get; set; }
+        public long FrozenBalance { get; set; }
+        public long Allowance { get; set; }
+
+        public Dictionary<string, AssetDto> Assets { get; set; }
+
+
+        public string String()
+        {
+            return JsonSerializerWrapper.Serialize(this);
+        }
+    }
+}
---
+++ kleversdk/provider/Dto/AssetDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using kleversdk.provider.Helper;
+
+namespace kleversdk.provider.Dto
+{
+    public class AssetDataDto
+    {
+        public AssetDto Asset { get; set; }
+    }
+
+    public class AssetDto
+    {
+        public string Address { get; set; }
+        public string AssetId { get; set; }
+        public long AssetType { get; set; }
+        public long Balance { get; set; }
+        public long Precision { get; set; }
+        public long FrozenBalance { get; set; }
+        public long UnfrozenBalance { get; set; }
+        public BuketDto[] Buckets { get; set; }
+
+
+        public string String()
+        {
+            return JsonSerializerWrapper.Serialize(this);
+        }
+    }
+
+    public class BuketDto
+    {
+        public string Id { get; set; }
+        public long StakedAt { get; set; }
+        public long StakedEpoch { get; set; }
+        public long UnstakedEpoch { get; set; }
+        public long Balance { get; set; }
+        public string Delegation { get; set; }
+        public string ValidatorName { get; set; }
+    }
+}
