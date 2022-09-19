6387b7834cbd2cfe9d838e338c9084ccec410dd7
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-19 18:40:25 -0300

    Constants

---
+++ kleversdk/core/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace kleversdk.core
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// Human-Readable Part
+        /// </summary>
+        public const string Hrp = "klv";
+
+        /// <summary>
+        /// KLV ticker
+        /// </summary>
+        public const string KLV = "KLV";
+
+        /// <summary>
+        /// KFI ticker
+        /// </summary>
+        public const string KFI = "KFI";
+
+        /// <summary>
+        /// HD Path prefix for BIP44
+        /// </summary>
+        public const string HDPrefix = "m/44'/690'/0'/0'";
+    }
+}
