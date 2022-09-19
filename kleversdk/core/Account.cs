216e9aea90187a4460b2ca20f39a162aff1ff550
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-19 18:44:30 -0300

    Address && Account

---
+++ kleversdk/core/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using kleversdk.provider;
+
+namespace kleversdk.core
+{
+    public class Account
+    {
+        public Address Address { get; }
+        public long Balance { get; private set; }
+        public long Nonce { get; private set; }
+
+        public Account(Address address)
+        {
+            Address = address;
+            Nonce = 0;
+            Balance = 0;
+        }
+
+        /// <summary>
+        /// Synchronizes account properties (such as nonce, balance) with the ones queried from the Network
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        public async Task Sync(IKleverProvider provider)
+        {
+            var acc = await provider.GetAccount(Address.Bech32);
+
+            Balance = acc.Balance;
+            Nonce = acc.Nonce;
+        }
+
+        /// <summary>
+        /// Increments (locally) the nonce (the account sequence number).
+        /// </summary>
+        public void IncrementNonce()
+        {
+            Nonce++;
+        }
+    }
+}
---
+++ kleversdk/core/Exceptions/AddressException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace kleversdk.core.Exceptions
+{
+    public class CannotCreateAddressException : Exception
+    {
+        public CannotCreateAddressException(string input)
+            : base($"Cannot create address from {input}")
+        {
+        }
+    }
+}
