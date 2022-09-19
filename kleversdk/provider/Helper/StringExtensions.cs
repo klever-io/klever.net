08961415977ee559f9620eef70d01680f3747448
Fernando Sobreira <fbsobreira@gmail.com>
2022-09-19 18:36:15 -0300

    Helper converter and extensions

---
+++ kleversdk/provider/Helper/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace kleversdk.provider.Helper
+{
+    public static class StringExtensions
+    {
+        public static string ToHex(this byte[] bytes)
+        {
+            return Converter.ToHexString(bytes).ToLowerInvariant();
+        }
+
+        public static byte[] FromHex(this string value)
+        {
+            return Converter.FromHexString(value);
+        }
+    }
+}
