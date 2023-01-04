6fb67e32ebfedc0f669805909533f2fc251746fa
Fernando Sobreira <fbsobreira@gmail.com>
2023-01-04 10:00:07 -0500

    KLC-143 create nuget package info (#2)
    * create nuget package info
    
    * Added all missing contracts (#1)
    
    * Add all contracts
    
    * misspelling
    
    * List to Dict for URIs TX request
    
    * Adjust demofor asset create
    
    * fix CreateValidator blsPublicKey
    add missing undelegate
    
    * fix CreateValidator blsPublicKey
    fix ConfigValidator blsPublicKey
    add missing contract undelegate
    
    Co-authored-by: Fernando Sobreira <fbsobreira@gmail.com>
    
    * update project and references to std lib
    
    Co-authored-by: CTJaeger <info@ct-j.de>

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using kleversdk.core;
 using kleversdk.provider;
 using kleversdk.provider.Dto;
@@ -12,7 +13,7 @@ namespace demo
         {
             
 
-            var kp = new KleverProvider(new NetworkConfig(Network.MainNet));
+            var kp = new KleverProvider(new NetworkConfig(Network.TestNet));
             Task<AccountDto> t = kp.GetAccount("klv1usdnywjhrlv4tcyu6stxpl6yvhplg35nepljlt4y5r7yppe8er4qujlazy");
             t.Wait();
             var result = t.Result;
@@ -21,7 +22,7 @@ namespace demo
 
 
             // create wallet with private key
-            //var wallet = new Wallet("HEX PRIVATE KEY");
+            // var wallet = new Wallet("HEX PRIVATE KEY");
             // Mnemonic
             var mnemonic = "word1 word2 ....";
             var wallet = Wallet.DeriveFromMnemonic(mnemonic);
@@ -35,7 +36,7 @@ namespace demo
             }
             catch(Exception e)
             {
-                Console.WriteLine("account does no exsist in blockchain yet: {0}", e.ToString());
+                Console.WriteLine("account does no exist in blockchain yet: {0}", e.ToString());
                 return;
             }
 
@@ -46,7 +47,7 @@ namespace demo
                 var tx = await kp.Send(acc.Address.Bech32, acc.Nonce, acc2.Address.Bech32, 100);
                 var decoded = await kp.Decode(tx);
                 var signature = wallet.SignHex(decoded.Hash);
-                tx.AddSiganture(signature);
+                tx.AddSignature(signature);
 
                 // broadcast
                 var broadcastResult = await kp.Broadcast(tx);
@@ -58,6 +59,59 @@ namespace demo
                 return;
             }
 
+            // Create Asset Test
+
+            StakingObject Staking;
+            Staking.apr = 1000L; // APR has 2 decimals precision use 1000L for 10%
+            Staking.minEpochsToClaim = 2L;
+            Staking.minEpochsToUnstake = 2L;
+            Staking.minEpochsToWithdraw = 2L;
+            Staking.type = 1;
+
+            var RoyalityInfo = new List<RoyalityInfo>();
+            RoyalityInfo.Add(new RoyalityInfo(amount: 100L, percentage: 100L));
+            RoyalityInfo.Add(new RoyalityInfo(amount: 200L, percentage: 150L));
+
+            Royaltiesobject Royalitys = new Royaltiesobject();
+            Royalitys.address = acc.Address.Bech32;
+            // MarketFixed and MarketPercentage can only be used for NFTs
+            //Royalitys.marketFixed = 10L; 
+            //Royalitys.marketPercentage = 10L;
+            Royalitys.transferFixed = 1000L;
+            Royalitys.transferPercentage = RoyalityInfo;
+
+            var Roles = new List<Role>();
+            Roles.Add(new Role("klv1nkyn8z4pxa88w7q8mdf6r92pjmwtm7e306td0vnsggml345ah3pq2l8z6n", HasRoleMint: true, HasRoleSetITOPrices: false));
+            
+            var Properties = new Propertiesobject();
+            Properties.canAddRoles = true;
+            Properties.canBurn = true;
+            Properties.canChangeOwner = false;
+            Properties.canFreeze = true;
+            Properties.canMint = true;
+            Properties.canPause = true;
+            Properties.canWipe = false;
+
+            var Attributes = new Attributesobject();
+            Attributes.isNFTMintStopped = false;
+            Attributes.isPaused = false;
+
+            var Uris = new Dictionary<string, string>();
+            Uris.Add("Twitter", "https://twitter.de");
+            try
+            {
+                var tx = await kp.CreateAsset("TestAsset", "TAS", acc.Address.Bech32, acc.Nonce, 6, Uris, "https://logos/logo1.png", 10000000, 10000000, 0, Staking, Royalitys, Roles, Properties, Attributes);
+                var decoded = await kp.Decode(tx);
+                string signature = wallet.SignHex(decoded.Hash);
+                tx.AddSignature(signature);
+                var broadcastResult = await kp.Broadcast(tx);
+                Console.WriteLine("Broadcast result: {0}", broadcastResult.String());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error sending: {0}", e.ToString());
+                return;
+            }
         }
     }
 }
